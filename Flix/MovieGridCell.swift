6622e3c42b4eb2f692a1d6a73c8c071d4d3d0a60
Yiheng Cen Feng <33232501+YihengCenFeng@users.noreply.github.com>
2020-02-26 20:06:15 -0500

    Add files via upload

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Yiheng Cen Feng on 2/26/20.
+//  Copyright © 2020 Yiheng. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+}
